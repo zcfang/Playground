bc69a21dffe6e0a607e8e8ebb1a09929456dc16a
zcfang <zhongcfang@gmail.com>
2019-12-10 15:19:53 -0800

    (refactor) Update solution so that space complexity is O(1)

--- LeetCode/sort/P905.cpp
+++ LeetCode/sort/P905.cpp
@@ -14,7 +14,7 @@ std::vector<int> sort_array_by_parity(std::vector<int> &A);
 
 int main(int argc, char const *argv[]) {
     std::vector<int> test_case_1{3,1,2,4};
-    std::vector<int> solution{2,4,3,1};
+    std::vector<int> solution{4,2,1,3};
 
     assert(sort_array_by_parity(test_case_1) == solution);
 
@@ -22,17 +22,18 @@ int main(int argc, char const *argv[]) {
 }
 
 std::vector<int> sort_array_by_parity(std::vector<int> &A) {
-    std::vector<int> output;
-    std::vector<int> odd;
+    int i = 0;
+    int j = A.size() - 1;
 
-    for (const int value : A) {
-        if (value % 2) {
-            odd.push_back(value);
-        } else {
-            output.push_back(value);
+    while (i < j) {
+        if (A[i] % 2) {
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
         }
+        if (!(A[i] % 2)) i++;
+        if (A[j] % 2) j--;
     }
-    output.insert(output.end(), odd.begin(), odd.end());
 
-    return output;
+    return A;
 }
