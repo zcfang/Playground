82ac302b3c0eb99c90e1352c442b7e8fdca26552
zcfang <zhongcfang@gmail.com>
2019-12-13 19:44:42 -0800

    (feat) Add solution to problem 215: Kth Largest Element in an Array

---
+++ LeetCode/heap/P215.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <assert.h>
+
+/**
+ * Problem from LeetCode 215: Kth Largest Element in an Array. You can find the
+ * problem
+ * <a href="https://leetcode.com/problems/kth-largest-element-in-an-array/">
+ * here</a>.
+ *
+ * @param[in] nums: A vector of unsorted numbers
+ * @param[in] k: kth largest number
+ * @returns The kth largest element in an unsorted array. Note that it is the
+ * kth largest element in the sorted order, not the kth distinct element.
+ */
+int find_kth_largest(std::vector<int> &nums, int k);
+
+int main(int argc, char const *argv[]) {
+    std::vector<int> nums{3,2,1,5,6,4};
+    int k = 2;
+
+    assert(find_kth_largest(nums, k) == 5);
+
+    return 0;
+}
+
+int find_kth_largest(std::vector<int> &nums, int k) {
+    std::sort(nums.begin(), nums.end(), std::greater<int>());
+        
+    return nums[k - 1];
+}
